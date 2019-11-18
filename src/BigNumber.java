a92f3c4bd8fbfb9b6436fbf0af264c58120017f4
thxwelchs <thxwelchs@gmail.com>
2019-11-18 16:28:56 +0900

    [UPDATE] BinNumber

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -2,6 +2,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * int -2147483647 ~ 2147483647
@@ -12,10 +13,10 @@ import java.util.List;
 public class BigNumber {
 
     public static void main(String[] args) {
-        sum();
-        multiplyByInt();
-        multiplyByBigInteger();
-        multiplication(Arrays.asList(6, 7, 8, 9), Arrays.asList(3, 4, 5));
+//        sum();
+//        multiplyByInt();
+//        multiplyByBigInteger();
+        multiplication(Arrays.asList(6, 7, 8, 9), Arrays.asList(2, 3, 6));
     }
 
     /**
@@ -62,28 +63,53 @@ public class BigNumber {
     }
 
     private static List<Integer> multiplication(List<Integer> a, List<Integer> b) {
-        List<Integer> result = new ArrayList<>();
+        List<Stack<Integer>> list = new ArrayList<>();
 
-        int temp = 0;
 
         for(int i = b.size()-1; i >= 0; i--) {
-            for(int j = a.size()-1; j >= 0; j++) {
+            int temp = 0;
+            Stack<Integer> stack = new Stack<>();
+
+            for(int j = 0; j < ((b.size() - 1) - i); j++) {
+                stack.push(0);
+            }
+
+            for(int j = a.size()-1; j >= 0; j--) {
                 int m = b.get(i) * a.get(j); // 각 원소의 값을 곱한다.
                 int remainder = m % 10; // 나머지
                 int quotient = m / 10; // 몫
 
-                int v = quotient + temp; // temp 값과 몫값을 더하여 실제 곱한 값을 추려낸다.
+                int v = remainder + temp;
+
+                temp = quotient;
+
                 if(v >= 10) {
-                    // 추려낸 값이 또 10이 넘을 수도 있으니 넘어간 값을 temp에 저장하고
-                    v = 10;
-                    temp = v - 10;
+                    v -= 10;
+                    temp++;
                 }
 
+                if(j == 0 && temp > 0) {
+                    stack.push(v);
+                    stack.push(temp);
+                } else {
+                    stack.push(v);
+                }
 
-                result.add(quotient + temp);
+//                for(int k = 0; k < (a.size() -1) - j; k++ ) {
+//                    
+//                }
 
             }
+
+//            System.out.println("iiiiii= " + i);
+//            System.out.println("b.size() -1 -j = " + ((b.size() -1) -i));
+
+            list.add(stack);
+
+            System.out.println("stack = " + stack);
         }
+
+        System.out.println("list = " + list);
 
         return new ArrayList<>();
     }
